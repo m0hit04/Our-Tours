689b5dc234c0bc8ad09ff34821cf1f8d4503a952
m0hit04 <msrathore0481@gmail.com>
2021-10-30 17:53:22 +0530

    added read more functinality

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 
 const Tour = ({ id, image, info, price, name }) => {
+  //states
+
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <article className="single-tour">
       <img src={image} alt={name}></img>
@@ -9,7 +13,13 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>{info}</p>
+        {/* Conditional Rendering */}
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "Show Less" : "Read More"}
+          </button>
+        </p>
         <button className="delete-btn">not interested</button>
       </footer>
     </article>
